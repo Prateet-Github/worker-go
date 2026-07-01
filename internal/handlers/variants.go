f4b451b9bef5f3ab13a3704ab447aa091e45f2a7
Prateet Tiwari <prateettiwari29@gmail.com>
2026-07-02 03:16:23 +0530

    Refactor generateVariants function to use goroutines with WaitGroup for concurrent variant generation

--- internal/handlers/variants.go
+++ internal/handlers/variants.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"path/filepath"
+	"sync"
 
 	"github.com/Prateet-Github/worker-go/internal/ffmpeg"
 )
@@ -14,21 +15,37 @@ func (h *VideoHandler) generateVariants(
 	outputDir string,
 ) error {
 
+	var wg sync.WaitGroup
+
+	errCh := make(chan error, len(ffmpeg.Renditions))
+
 	for _, rendition := range ffmpeg.Renditions {
+		r := rendition
 
-		renditionDir := filepath.Join(
-			outputDir,
-			rendition.Name,
-		)
+		wg.Go(func() { // new syntax for WaitGroup in Go 1.25 intead of wg.Add(1) and defer wg.Done() & go func()
+			renditionDir := filepath.Join(
+				outputDir,
+				r.Name,
+			)
 
-		log.Printf("Generating %s...", rendition.Name)
+			log.Printf("Generating %s...", r.Name)
 
-		if err := h.ffmpeg.GenerateVariant(
-			ctx,
-			inputPath,
-			renditionDir,
-			rendition,
-		); err != nil {
+			if err := h.ffmpeg.GenerateVariant(
+				ctx,
+				inputPath,
+				renditionDir,
+				r,
+			); err != nil {
+				errCh <- err
+			}
+		})
+	}
+
+	wg.Wait()
+	close(errCh)
+
+	for err := range errCh {
+		if err != nil {
 			return err
 		}
 	}
